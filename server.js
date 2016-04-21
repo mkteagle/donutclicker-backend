ddf8a7c7e0330c1debe0b0995e9d52b5268b61ae
Michael Teagle <mkteagle@gmail.com>
2016-04-21 08:46:19 -0600

    committing some changes

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+var ObjectId = require('mongodb').ObjectID;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://54.191.202.80:27017/donut-clicker';
+app.use('/', express.static(__dirname + '/www'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/api', router);
+var port = (process.env.PORT || 5000);
+
+app.listen(port, function() {
+  console.log(`App listening on port ${port}...`);
+});
